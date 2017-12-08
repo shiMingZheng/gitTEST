4296402f6da76a05e828321bc4ba5e2ed4f780e7
shiMingZheng <31204914+shiMingZheng@users.noreply.github.com>
2017-12-08 17:11:02 +0800

    this is a branch

--- src/hello.java
+++ src/hello.java
@@ -3,7 +3,7 @@
  */
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world,this is a branch");
         System.out.println("hello world");
     }
 }
